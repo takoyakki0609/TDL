3af71462dd59626cd2c1dea3313fd739a9f12236
takoyakki0609 <kjy6478@naver.com>
2023-11-23 14:30:21 +0900

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,145 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import shortid from "shortid";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  return <div></div>;
+  const dispatch = useDispatch();
+  const taskList = useSelector((state) => state.user);
+  const [task, setTask] = useState([
+    { id: shortid.generate(), title: "", content: "", isDone: false },
+  ]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onContentHandler = (e) => {
+    setContent(e.target.value);
+  };
+
+  const AddTaskSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      id: shortid.generate(),
+      title,
+      content,
+      isDone: false,
+    };
+    setTask([newTask, ...task]);
+  };
+
+  const handleClickDelete = (item) => {
+    const deleteTask = task.filter((todo) => {
+      return item.id !== todo.id;
+    });
+    setTask(deleteTask);
+  };
+
+  const handleClicked = (item) => {
+    const todo = task.map((todo) => {
+      if (item.id === todo.id)
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      else return todo;
+    });
+    setTask(todo);
+  };
+
+  console.log(task);
+  return (
+    <Container>
+      <Header>
+        <h1>Todo List</h1>
+      </Header>
+      <AddTask onSubmit={AddTaskSubmit}>
+        <label>제목</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={onTitleHandler}
+        />
+        <label>내용</label>
+        <input
+          type="text"
+          name="content"
+          value={content}
+          onChange={onContentHandler}
+        />
+        <button type="submit">등록하기</button>
+      </AddTask>
+      <OngoingBox>
+        <h2>진행중</h2>
+        <Ongoing>
+          {task
+            .filter((item) => {
+              return item.isDone === false;
+            })
+            .map((item) => {
+              return (
+                <div key={item.id}>
+                  <h3>{item.title}</h3>
+                  <p>{item.content}</p>
+                  <button
+                    onClick={() => {
+                      handleClicked(item);
+                    }}
+                  >
+                    완료
+                  </button>
+                  <button onClick={() => handleClickDelete(item)}>삭제</button>
+                </div>
+              );
+            })}
+        </Ongoing>
+      </OngoingBox>
+      <DoneBox>
+        {task
+          .filter((item) => {
+            return item.isDone === true;
+          })
+          .map((item) => {
+            return (
+              <div key={item.id}>
+                <h3>{item.title}</h3>
+                <p>{item.content}</p>
+                <button
+                  onClick={() => {
+                    handleClicked(item);
+                  }}
+                >
+                  되돌리기
+                </button>
+                <button onClick={() => handleClickDelete(item)}>삭제</button>
+              </div>
+            );
+          })}
+      </DoneBox>
+    </Container>
+  );
 };
 
 export default Home;
+
+const Container = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1200px;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const AddTask = styled.form``;
+
+const OngoingBox = styled.div``;
+const Ongoing = styled.div``;
+const DoneBox = styled.div``;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -5,16 +5,36 @@ const initialState = [
   {
     id: shortid.generate(),
     title: "",
-    body: "",
+    content: "",
     isDone: false,
   },
 ];
+
+const ADD_TODO = 'todos/ADD_TODO'
+
+const DELETE_TODO='todos/DELETE_TODO'
+
+const SWITCH_TODO = 'todos/SWITCH_TODO'
+
+export const addToto = (payload) => {
+  return {type:ADD_TODO, payload}
+}
+
+export const deleteTodo = (payload) => {
+  return {type:DELETE_TODO, payload}
+}
+
+export const switchTodo = (payload) => {
+  return {type:SWITCH_TODO, payload}
+}
 
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return; //TODO: 여기 작성
+      const newAdd = action.payload;
+      return [newAdd, ...state]
+      //TODO: 여기 작성
 
     case "DELETE_TODO":
       return; //TODO: 여기 작성
